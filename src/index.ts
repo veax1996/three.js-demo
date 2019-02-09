96277459dd93ce464ad118d687890263c57f9bf5
lingjun <lingjun@qunhemail.com>
2019-02-09 17:35:08 +0800

    opt code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import sphereScene from './scene/sphereGeometry';
-import shodowScene from './scene/shodow';
+import shodowScene from './scene/shadow';
 
 //相机
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
